f47b9bb3a1830a98ee0df2cb48bd0bd062acf4ab
Andres <impire.suprem@gmail.com>
2026-05-01 15:11:46 -0400

    implementacion del login

--- Cliente/src/App.jsx
+++ Cliente/src/App.jsx
@@ -2,6 +2,10 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Login from './components/Login'
+import Register from './components/Register'
+import ResetPassword from './components/ResetPassword'
+
 
 function App() {
   return (
@@ -9,6 +13,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
+        <Route path="/ResetPassword" element={<ResetPassword />} />
       </Routes>
       <Footer />
     </>
