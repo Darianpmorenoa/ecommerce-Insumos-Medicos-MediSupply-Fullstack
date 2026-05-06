4b755fe800fdb9fb9d2ee7b152d051f5af1adb7c
Darianpmorenoa <darianmorenis@gmail.comgit config --global user.email darianmorenis@gmail.com>
2026-05-05 20:20:22 -0400

    Backend: Inicialización del proyecto e instalación de dependencias

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// Middlewares globales
+app.use(cors()); 
+app.use(express.json());
+
+// Rutas aquí:
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`SERVER ON PORT ${PORT}`);
+});
+
+module.exports = app;
