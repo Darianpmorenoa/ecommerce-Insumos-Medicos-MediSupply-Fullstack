1b86d325f9cc737f22a7227bbea2b1854e262d66
Andres <impire.suprem@gmail.com>
2026-05-16 05:14:57 -0400

    arreglando ruta de DB

--- Cliente/src/api/api.js
+++ Cliente/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 1.URL base usando la variable de entorno de Render
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api';
 
 // 2. Instancia personalizada de axios
 const clienteAxios = axios.create({
